01cf205c59b5f788c65312f8f6943447602834f3
Tristan Ferrara <43273055+tristanferrara@users.noreply.github.com>
2019-10-04 12:00:23 -0400

    Create Skateboard.h
    Created Skateboard.h and associated code.

---
+++ Skateboard.h
@@ -0,0 +1,22 @@
+//
+// Created by Esteban Parra on 9/5/19.
+//
+
+#ifndef DRIVINGSIMULATOR_SKATEBOARD_H
+#define DRIVINGSIMULATOR_SKATEBOARD_H
+
+#include "Vehicle.h"
+
+class Skateboard : public Vehicle {
+
+public:
+    explicit Skateboard(string brand, string model);
+
+    virtual ~Skateboard();
+    virtual double mileageEstimate(double time);
+
+    virtual string toString();
+};
+
+
+#endif //DRIVINGSIMULATOR_Skateboard_H
